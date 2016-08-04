0296db9fddb40c5c56a595ceae20d7acb6a679c8
jennikins813 <jennikins813@yahoo.com>
2016-08-04 10:43:04 -0700

    feat: Implement Player class; define allEnemies object

--- js/app.js
+++ js/app.js
@@ -24,12 +24,25 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-horn-girl.png';
+}
+Player.prototype.update = function(){
+    
+}
+Player.prototype.render = function(){
+    
+}
+Player.prototype.handleInput = function(){
+    console.log("HandleInput");
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your

50fdb7a19d7e5f9d415f322fadbea5ffe228f9d4
jennikins813 <jennikins813@yahoo.com>
2016-08-04 19:46:04 -0700

    feat: Add coordinates for enemy objects; enemies now rendered on page

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -31,7 +33,7 @@ Player.prototype.update = function(){
     
 }
 Player.prototype.render = function(){
-    
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  //same as Enemy render
 }
 Player.prototype.handleInput = function(){
     console.log("HandleInput");
@@ -41,7 +43,11 @@ Player.prototype.handleInput = function(){
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
+var enemy1 = new Enemy(50, 50);
+var enemy2 = new Enemy(150, 135);
+var enemy3 = new Enemy(250, 220);
+
+var allEnemies = [enemy1, enemy2, enemy3];
 var player = new Player();
 
 
